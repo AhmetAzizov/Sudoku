746bab6a71db8eab19072978e9a6de0c6c389371
AhmetAzizov <ahmet.azizov@gmail.com>
2022-11-16 03:00:44 +0100

    Structure Change
    Altered the project to add most elements by DOM manipulation directly from JavaScript

--- script.js
+++ script.js
@@ -4,30 +4,156 @@ var busy = false;
 var test2 = 0;
 
 
-  // mainbody contains the HTML content of "mainBody". We delete the content of mainBody the moment user presses the start button. 
-  // We stored the code in a variable in order to use it again when the user finishes the game or just presses the menu button.
-  // it might seem like a bad idea but it works! For now atleast.
- var mainbody = '<button id="startButton" onclick="startButton()"><b>Start</b></button>';
-    mainbody += '<select id="selectDifficulty">';
-    mainbody += '<option selected value="0" disabled hidden>------Difficulty------</option>';
-    mainbody += '<option value="3">Easy</option>';
-    mainbody += '<option value="4">Medium</option>';
-    mainbody += '<option value="5">Hard</option></select>';
-    mainbody += '<select id="selectFormat">';
-    mainbody += '<option selected value="0" disabled hidden>------Format------</option>';
-    mainbody += '<option value="2,3">6 x 6</option>';
-    mainbody += '<option value="3,3">9 x 9</option></select>';
+function createStartPage(){
+  const body = document.body;
 
-    document.getElementById("mainBody").innerHTML = mainbody;
+  var startButtonn = document.createElement("button");
+  startButtonn.id = "startButton";
+  //startButtonn.onclick = startButton();
+  startButtonn.onclick = () => {startButton()};
+  startButtonn.innerText = "START";
+
+  var selectDifficulty = document.createElement("select");
+  selectDifficulty.id = "selectDifficulty";
+  
+  var optionDefault = document.createElement("option");
+  optionDefault.selected = true;
+  optionDefault.value = "0";
+  optionDefault.disabled = true;
+  optionDefault.hidden = true;
+  optionDefault.innerText = "------Difficulty------";
+
+  const optionEasy = document.createElement("option");
+  optionEasy.value = "3";
+  optionEasy.innerText = "Easy";
+
+  const optionMedium = document.createElement("option");
+  optionMedium.value = "4";
+  optionMedium.innerText = "Medium";
+
+  const optionHard = document.createElement("option");
+  optionHard.value = "3";
+  optionHard.innerText = "Hard";
+
+  selectDifficulty.append(optionDefault, optionEasy, optionMedium, optionHard);
+
+
+  var selectFormat = document.createElement("select");
+  selectFormat.id = "selectFormat";
+
+  const optionFormatDefault = document.createElement("option");
+  optionFormatDefault.value = "0";
+  optionFormatDefault.selected = true;
+  optionFormatDefault.innerText = "------Format------";
+
+  const option6x6 = document.createElement("option");
+  option6x6.value = "2,3";
+  option6x6.innerText = "6 x 6";
+
+  const option9x9 = document.createElement("option");
+  option9x9.value = "3,3";
+  option9x9.innerHTML = "9 x 9";
+
+  selectFormat.append(optionFormatDefault, option6x6, option9x9)
+  
+  body.append(startButtonn, selectDifficulty, selectFormat);
+}
+
+function deleteStartPage(){
+  document.getElementById("startButton").remove();
+  document.getElementById("selectFormat").remove();
+  document.getElementById("selectDifficulty").remove();
+}
+
+function deleteMainPage(){
+  document.getElementById("sudokuContainer").remove();
+  document.getElementById("sideGroupBox").remove();
+}
+
+function createMainPage(format){
+  var buttonPopup = document.createElement("div");
+	buttonPopup.id = "buttonPopup"
+	
+	for (var a = 1; a < format + 1; a++) {
+      var button1 = document.createElement("div");
+			button1.id = `button${a}`;
+			button1.classList.add("button");
+			button1.innerHTML = `${a}`;
+			
+			buttonPopup.append(button1);
+    	}
+		
+	var clearButton = document.createElement("div");
+	clearButton.id = "clearButton";
+	clearButton.classList.add("button");
+	clearButton.innerHTML = "clear";
+	
+	var tipButton = document.createElement("div");
+	tipButton.id = "tipButton";
+	tipButton.classList.add("button");
+	tipButton.innerHTML = "&#128161;";
+	
+	buttonPopup.append(clearButton, tipButton);
+	
+	var sideGroupBox = document.createElement("div");
+	sideGroupBox.id = "sideGroupBox";
+	
+	var menuButton = document.createElement("div");
+	menuButton.id = "menuButton";
+	menuButton.classList.add("sideButton");
+	menuButton.innerHTML = "&#127968;";
+
+  var sideTipButton = document.createElement("div");
+  sideTipButton.id = "sideTipButton";
+  sideTipButton.classList.add("sideButton");
+  sideTipButton.innerHTML = "&#128161; 0";
+	
+	var solveButton = document.createElement("div");
+	solveButton.id = "solveButton";
+	solveButton.classList.add("sideButton");
+	solveButton.innerHTML = "Solve the Game";
+	
+	var pauseButton = document.createElement("div");
+	pauseButton.id = "pauseButton";
+	pauseButton.classList.add("sideButton");
+	pauseButton.innerHTML = "&#9208;";
+	
+	var timer = document.createElement("div");
+	timer.id = "timer";
+	timer.classList.add("sideButton");
+	timer.innerHTML = "00:00";
+	
+	sideGroupBox.append(menuButton, sideTipButton, solveButton, pauseButton, timer);
+
+  var sudokuContainer = document.createElement("div");
+  sudokuContainer.id = "sudokuContainer";
+	
+	for (var a = 1; a < format + 1; a++) {
+      for (var b = 1; b < format + 1; b++) {
+        var inputOdd = Math.ceil(a / boxRow) % 2 ^ Math.ceil(b / boxCol) % 2;
+        
+        var buttons = document.createElement("div");
+        buttons.id = `a${a};${b}`;
+        buttons.classList.add("input");
+        buttons.classList.add(`input${inputOdd}`);
+        
+        sudokuContainer.append(buttons);
+      }
+    }
+	
+  sudokuContainer.append(buttonPopup);
+	document.body.append(sideGroupBox, sudokuContainer);
+}
+
+createStartPage();
 
 // this is the function that is invoked when the user presses start button 
-function startButton() {
+function startButton(){
   if (busy) {
     return;
   }
   busy = true;
   try {
-
     // error message is shown if the user decides to press the start button without selecting difficulty, format or neither of them. Error message will be shown accordingly 
     if (document.getElementById("selectDifficulty").value == "0" && document.getElementById("selectFormat").value == "0") {
       alert("both difficulty and Format aren't selected");
@@ -52,7 +178,6 @@ function startButton() {
       return row + ";" + col;
     }
 
-
     var format = boxRow * boxCol; // this variable stores the information whether the game will be 6x6 or 9x9 by simply multiplying the two sides.
 
     // this array contains numbers from 1 to 6 or 9 depending on the format user has chosen.
@@ -61,37 +186,7 @@ function startButton() {
       __random.push(index + 1);
     }
 
-    // variable html2 contains the HTML code of the base game in "sudokuContainer" just like mainbody.
-    var html2 = '<div id="sudokuContainer">';
-    html2 += '<div id="buttonPopup">';
-
-    //html2 += '<div id="buttonPopupTriangle"></div>';
-
-    for (var a = 1; a < format + 1; a++) {
-      html2 += `<div id="button${a}" class="button">${a}</div>`;
-    }
-
-    html2 += '<div id="clearButton" class="button">clear</div>';
-    html2 += '<div id="tipButton" class="button">&#128161;</div>';
-    html2 += "</div>";
-
-    for (var a = 1; a < format + 1; a++) {
-      for (var b = 1; b < format + 1; b++) {
-        var inputOdd = Math.ceil(a / boxRow) % 2 ^ Math.ceil(b / boxCol) % 2;
-        html2 += `<div class="input input${inputOdd}" id="a${getIndex(a, b)}"></div>`;
-      }
-    }
-    html2 += '</div>'
-
-    html2 += '<div id="sideGroupBox">';
-    html2 += '<div id="menuButton" class="sideButton">&#127968;</div>';
-    html2 += '<div id="solveButton" class="sideButton">Solve the Game</div>';
-    html2 += '<div id="tipButton" class="sideButton">&#128161; 0</div>';
-    html2 += '<div id="timer" class="sideButton">00:00</div>';
-    html2 += '<div id="pauseButton" class="sideButton">&#9208;</div></div>';
-    
-    
-    document.getElementById("startingPage").innerHTML = html2; // we insert the content of html2 to sudokuContainer.
+    createMainPage(format);
     
     // here, we set the width of the container according to the format of the game. We do this to ensure the boxes inside the container are fitted correctly.
     document.getElementById("sudokuContainer").style.width = `${format * 100}px`; 
@@ -121,34 +216,31 @@ function startButton() {
           }
         }
 
-        if (Object.keys(sudokuMatrix).length == format ** 2 && error == 0) {
-          console.log("Congratulations! You have finished the game");
+        var inputs = document.getElementsByClassName("input");
+        for (let index = 0; index < inputs.length; index++) {
+          const element = inputs[index];
+          element.classList.remove("cellHover");
+          element.classList.remove("cellActive");
+        }
 
+
+        if (Object.keys(sudokuMatrix).length == format ** 2 && error == 0) {
           document.getElementById("endScreen").style.visibility = "visible";
+          document.getElementById("endScreenFinishTime").innerHTML = minutesDisplay + ":" + secondsDisplay;
           clearInterval(timer);
         }
       }
     }
-    
-    // to change color of popup buttons when we hover over
-    var buttons = document.getElementsByClassName("button");
-      for (let index = 0; index < buttons.length - 1; index++) { // we put buttons.length - 1 to not affect tip button
-        const element = buttons[index];
-        
-        element.addEventListener("mouseover", () => (element.style.backgroundColor = "rgb(85, 135, 154)"))
-        element.addEventListener("mouseout", () => (element.style.backgroundColor = "rgb(133, 208, 235"))
-      }
-
-
-
 
     document.getElementById("menuButton").onclick = () => {
-      document.getElementById("startingPage").innerHTML = "";
-      document.getElementById("mainBody").innerHTML = mainbody; 
+      document.getElementById("sudokuContainer").innerHTML = "";
+      //document.getElementById("mainBody").innerHTML = mainbody; 
+
+      createStartPage();
+      deleteMainPage();
 
       clearInterval(timer);
     }
-
 
     document.getElementById("solveButton").onclick = () => {
       for (const key in matrixClone) {
@@ -166,17 +258,11 @@ function startButton() {
       clearInterval(timer);
     }
 
-    document.getElementById("tipButton").onclick = () => {
-      console.log(matrixClone[getIndex(num1, num2)]);
-      document.getElementById(`a${getIndex(num1, num2)}`).innerText = matrixClone[getIndex(num1, num2)];
-    }
-
-
     document.getElementById("pauseButton").onclick = () => {
       // document.getElementById("sudokuContainer").style.visibility = "hidden";
       document.getElementById("pauseScreen").style.visibility = "visible";
 
-      document.getElementById("startingPage").style.pointerEvents = "none";
+      document.getElementById("sudokuContainer").style.pointerEvents = "none";
 
       document.getElementById("pauseScreenTimeLeft").innerHTML = minutesDisplay + ":" + secondsDisplay;
 
@@ -187,14 +273,14 @@ function startButton() {
       // document.getElementById("sudokuContainer").style.visibility = "visible";
       document.getElementById("pauseScreen").style.visibility = "hidden";
 
-      document.getElementById("startingPage").style.pointerEvents = "all";
+      document.getElementById("sudokuContainer").style.pointerEvents = "all";
 
       isPaused = false;
     }
 
     document.getElementById("endScreenHomeButton").onclick = () => {
-      document.getElementById("startingPage").innerHTML = "";
-      document.getElementById("mainBody").innerHTML = mainbody; 
+      deleteMainPage();
+      createStartPage();
 
       document.getElementById("endScreen").style.visibility = "hidden";
     }
@@ -203,13 +289,6 @@ function startButton() {
       document.getElementById("endScreen").style.visibility = "hidden";
       
       clearInterval(timer);
-    }
-
-    function checkIfNull() {
-      for (var element in sudokuMatrix) {
-        if (element == null) return false;
-        else return true;
-      }
     }
 
     document.getElementById("clearButton").onclick = () => {
@@ -226,6 +305,13 @@ function startButton() {
             document.getElementById(`a${getIndex(a, b)}`).classList.remove("cellError");
         }
       }
+
+      var inputs = document.getElementsByClassName("input");
+        for (let index = 0; index < inputs.length; index++) {
+          const element = inputs[index];
+          element.classList.remove("cellHover");
+          element.classList.remove("cellActive");
+        }
     }
     
 
@@ -236,8 +322,6 @@ function startButton() {
     function onchangeHandler(i, j) {
       $(`a${getIndex(i, j)}`).addEventListener("click", (e) => {
         document.getElementById("buttonPopup").style.visibility = "visible";
-
-        //document.getElementById("buttonPopup").style.transition = "10ms";
 
         lightRowsAndColumns(i, j, e.target);
 
@@ -252,9 +336,15 @@ function startButton() {
       });
     }
 
-    //document.getElementById("buttonPopup").addEventListener("transitionend", () => (document.getElementById("buttonPopup").style.transition = "10ms"))
-    
-
+    // to change color of popup buttons when we hover over
+    var buttons = document.getElementsByClassName("button");
+      for (let index = 0; index < buttons.length - 1; index++) { // we put buttons.length - 1 to not affect tip button
+        const element = buttons[index];
+        
+        element.addEventListener("mouseover", () => (element.style.backgroundColor = "rgb(85, 135, 154)"))
+        element.addEventListener("mouseout", () => (element.style.backgroundColor = "rgb(133, 208, 235"))
+    }
+-
 
     function toggleRight(){
       num2++;
@@ -303,17 +393,10 @@ function startButton() {
       e.classList.add("cellActive");
     }
 
-    const buttonPopupElement = document.getElementById("buttonPopup");
-
-    buttonPopupElement.addEventListener("mouseover", () => (buttonPopupElement.style.transition = "height 500ms"))
-
-    buttonPopupElement.addEventListener("mouseout", () => (buttonPopupElement.style.transition = "none"))
   
 
     document.addEventListener("click", function (e) {
       if (!document.getElementById("sudokuContainer").contains(e.target) || document.getElementById("sideGroupBox").contains(e.target)){
-        //document.getElementById("buttonPopup").style.transition = "500ms";
-
         var inputs = document.getElementsByClassName("input");
         for (let index = 0; index < inputs.length; index++) {
           const element = inputs[index];
@@ -457,7 +540,7 @@ function startButton() {
       document.getElementById(`a${key}`).classList.add("cellInitial");
     }
 
-    document.getElementById("mainBody").innerHTML = "";
+    // document.getElementById("mainBody").innerHTML = "";
 
     // ---------------------------------------------------------------------
 
@@ -495,6 +578,7 @@ function startButton() {
     }
     }
 
+    deleteStartPage();
   } finally {
     busy = false;
   }
